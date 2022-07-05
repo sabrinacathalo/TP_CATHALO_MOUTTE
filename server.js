ea4b75f846ce9522e11318fdc6c18783387c6c52
sabrinacathalo <sabrina.cathalo@epsi.fr>
2022-07-05 17:18:36 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const HTTP_PORT = 8000;
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Server running on port ${HTTP_PORT}`)
+});
+
+//Basic route
+app.get('/', (req, res) => {
+    res.json({message: 'Hello World'});
+});

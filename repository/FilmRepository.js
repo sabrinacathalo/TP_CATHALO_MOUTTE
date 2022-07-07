451edea3ae2e05ff5a5e1af18c3557297b5e2d8a
Drreams <florianmoutte1@hotmail.fr>
2022-07-07 10:09:06 +0200

    film

---
+++ repository/FilmRepository.js
@@ -0,0 +1,101 @@
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+class FilmRepository {
+    constructor(database) {
+        this.database = database;
+    }
+
+    list() {
+        return new Promise((resolve, reject) => {
+            this.database.all('SELECT * FROM films', [], (err, rows) => {
+                if (err) {
+                    console.error(err.message);
+                    reject(err);
+                } else {
+                    resolve(
+                        rows,
+                    );
+                }
+            });
+        });
+    }
+    get(id) {
+        return new Promise((resolve, reject) => {
+            this.database.get('SELECT * FROM film WHERE id = ?', [id], (err, row) => {
+                if (err) {
+                    console.error(err.message);
+                    reject(err);
+                } else {
+                    resolve(
+                        this.decorator(row),
+                    );
+                }
+            });
+        });
+    }
+
+    create(data) {
+        return new Promise((resolve, reject) => {
+            this.database.run(
+                'INSERT INTO film (contents, done) VALUES (?,?)',
+                [data.contents, data.done ? 1 : 0],
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                        reject(err);
+                    } else {
+                        resolve(this.lastID);
+                    }
+                },
+            );
+        });
+    }
+
+    update(id, data) {
+        return new Promise((resolve, reject) => {
+            this.database.run(
+                `UPDATE film
+                 SET contents = ?,
+                     done = ?
+                 WHERE id = ?`,
+                [data.contents, data.done ? 1 : 0, id],
+                (err) => {
+                    if (err) {
+                        console.error(err.message);
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                },
+            );
+        });
+    }
+
+    delete(id) {
+        return new Promise((resolve, reject) => {
+            this.database.run(
+                `DELETE FROM film
+                 WHERE id = ?`,
+                [id],
+                (err) => {
+                    if (err) {
+                        console.error(err.message);
+                        reject(err);
+                    } else {
+                        resolve(true);
+                    }
+                },
+            );
+        });
+    }
+/*
+    // eslint-disable-next-line class-methods-use-this
+    decorator(todo) {
+        return {
+            ...todo,
+            done: todo.done === 1,
+        };
+    }*/
+}
+
+module.exports = FilmRepository;
--- route.js
+++ route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ActorController = require('./controllers/ActorController');
-const GenreController = require('./controllers/GenreController')
+const GenreController = require('./controllers/GenreController');
+const FilmController = require('./controllers/FilmController')
 
 const router = express.Router();
 
@@ -13,5 +14,11 @@ router.delete('/actor/:id', ActorController.delete);
 router.get('/genre', GenreController.list);
 router.post('/genre', GenreController.create);
 router.delete('/genre/:id', GenreController.delete);
+
+router.get('/film', FilmController.list);
+/*router.get('/film/:id', FilmController.get);
+router.post('/film', FilmController.create);
+router.put('/film/', FilmController.update);
+router.delete('/film/:id', FilmController.delete);*/
 
 module.exports = router;
